24f7a26b76558fa1c5bd317ddafc6b500c6ca6dd
ow3ndesu <enguerraowen@gmail.com>
2023-01-20 12:27:31 +0800

    fixed routes

--- api/routes/passwordRoutes.php
+++ api/routes/passwordRoutes.php
@@ -1,8 +1,9 @@
 <?php
     include_once("../process/passwordProcess.php");
     $process = new Process();
+    $data = json_decode(file_get_contents('php://input'), true);
 
-    if(isset($_POST["action"]) && $_POST["action"] == "md5Generator"){
-        $process->md5Generator($_POST);
+    if(isset($data["action"]) && $data["action"] == "md5Generator"){
+        $process->md5Generator($data);
     }
 ?>
